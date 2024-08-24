6623d60c121972807dc829109f0f47902012e57c
Talah Shehzad <talahshehzad7@gmail.com>
2024-08-24 09:58:01 -0700

    Upadated EditProfile page

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "../css/editprofile.css";
 
-function EditProfile() {
-  return <div>EditProfile</div>;
-}
+const EditProfile = () => {
+  return (
+    <div className="edit-profile-container">
+      <h2>Edit Profile</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <button type="submit">Save Changes</button>
+      </form>
+    </div>
+  );
+};
 
 export default EditProfile;
